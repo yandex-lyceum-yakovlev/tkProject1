8d45c131651774824bad3bcda528040083d157a8
yandex-lyceum-yakovlev <yakovlev@yandexlyceum.ru>
2023-01-11 16:22:16 +0400

    grid

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ import tkinter
 w = tkinter.Tk()
 c = tkinter.Canvas(width=600, height=600, background="white")
 c.pack()
+for i in range(50, 600, 50):
+    c.create_line((i, 0), (i, 600), fill="red")
+    c.create_line((0, i), (600, i), fill="red")
+# c.create_rectangle((100, 100), (200, 300), outline="green")
+# c.create_oval((100, 100), (200, 300), outline="blue")
 w.mainloop()
